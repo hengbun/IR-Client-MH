ab01637f9aa564fc9bb4794dcd816b6c40b6bd75
HengBun <hengkybuntoro@gmail.com>
2017-03-30 01:33:17 +0700

    Initial clone from Bitbucket

---
+++ InputRedirectionNTR/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InputRedirectionNTR
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        public static NTRClient ntrClient;
+        public static ScriptHelper scriptHelper;
+        public static Boolean Connected = false;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ntrClient = new NTRClient();
+            scriptHelper = new ScriptHelper();
+
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+#endif
+}
